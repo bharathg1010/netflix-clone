cc08efa1fbff9bedbc98275e14876895e8fcd7af
bharathg1010 <bhagoli@BHAGOLI-LAP.in.oracle.com>
2021-02-04 16:15:04 +0530

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNou3xdYcLSQTk44Lv138MxpyOFUvHmKk",
+    authDomain: "netflix-clone-d4bf2.firebaseapp.com",
+    projectId: "netflix-clone-d4bf2",
+    storageBucket: "netflix-clone-d4bf2.appspot.com",
+    messagingSenderId: "205993937945",
+    appId: "1:205993937945:web:5c0bc07760d9c1bdcaee88"
+  };
